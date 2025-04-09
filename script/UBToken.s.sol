b3d0760f0c21ecbd6e8e84735ffebd948e833b58
YoussefHassanAWS <yhassan@my.bridgeport.edu>
2025-04-09 16:16:54 -0400

    final

--- script/UBToken.s.sol
+++ script/UBToken.s.sol
@@ -2,23 +2,21 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
-import {UBToken} from "/Users/youssefhassan/tokens/src/UBToken.sol";
+import "../src/UBToken.sol";
 
 contract UBTokenDeploy is Script {
-
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-
+        
         vm.startBroadcast(deployerPrivateKey);
 
         string memory tokenName = "Universitity or Bridgeport Token";
         string memory tokenSymbol = "UBT";
-        uint8 decimals = 18;
-        uint256 initialSupply = 1_000_000 * 10 ** decimals;
+        uint256 initialSupply = 1_000_000 * 10 ** 18;
 
-        UBToken token = new UBToken(tokenName, tokenSymbol, decimals, initialSupply);
+        UBToken token = new UBToken(tokenName, tokenSymbol, initialSupply);
 
-        token.transfer(0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc, 100 * 10 ** decimals);
+        token.transfer(0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc, 100 * 10 ** 18);
 
         vm.stopBroadcast();
     }
